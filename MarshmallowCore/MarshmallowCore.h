2f8a03137f0bda3b18578b5b68ff0a43950c0bd9
Kelly Huberty <kellyhuberty@gmail.com>
2014-02-28 02:31:14 -0600

    Changes to Licensing and podspec...

--- MarshmallowCore/MarshmallowCore.h
+++ MarshmallowCore/MarshmallowCore.h
@@ -6,8 +6,30 @@
 //  Copyright (c) 2014 Kelly Huberty. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+//#import <Foundation/Foundation.h>
+#import "MMDebug.h"
+#import "MMInit.h"
+#import "MMPreferences.h"
+#import "MMSet.h"
 
-@interface MarshmallowCore : NSObject
+/**
+ */
+#import "MMUtility.h"
 
-@end
+
+/**
+ Check whether a file at a given URL has a newer timestamp than a given file.
+ Example usage:
+ @code
+ NSURL *url1, *url2;
+ BOOL isNewer = [FileUtils
+ isThisFileNewerThanThatFile:url1 thatURL:url2];
+ @endcode
+ @see http://www.dadabeatnik.com for more information.
+ @param thisURL
+ The URL of the source file.
+ @param thatURL
+ The URL of the target file to check.
+ @return YES if the timestamp of @c thatURL is newer than the timestamp of @c thisURL,
+ otherwise NO.
+ */
